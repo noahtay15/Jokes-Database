0189c3ac21bf5699d4402e02465f5d863aa2282c
noahtay15 <noahtay34@gmail.com>
2023-07-06 11:36:57 -0400

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Jokes_Database_Web_App.Models;
+
+namespace Jokes_Database_Web_App.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Jokes_Database_Web_App.Models.Joke>? Joke { get; set; }
+    }
+}
